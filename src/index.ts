4497c565ad8a05c7ac56ef149c9ed08b5aef8263
Huy Vuong <huy-vuong@users.noreply.github.com>
2024-02-11 22:42:55 -0800

    feat: set up basic Fastify server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
+import Fastify from 'fastify';
 import { getHello } from 'template-ts-node-fastify/hello';
 
-console.log(getHello());
+const fastify = Fastify({ logger: true });
+
+fastify.get('/', async (request, reply) => {
+  reply.send({
+    message: getHello(),
+    method: request.method,
+    url: request.url,
+    query: request.query,
+    headers: request.headers,
+  });
+});
+
+fastify.post('/', async (request, reply) => {
+  reply.send({
+    message: getHello(),
+    method: request.method,
+    url: request.url,
+    query: request.query,
+    headers: request.headers,
+    body: request.body,
+  });
+});
+
+async function start() {
+  try {
+    fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+start().then(() => {});
